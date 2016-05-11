bda2233669da29ca700d5c6aae3b8a6cb6a10e48
Nivade <nhavandeursen@gmail.com>
2016-05-11 18:34:14 +0200

    Add style/js links for bootstrap,font-awesome,jquery,angular and less.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Niels van Deursen</title>
+  <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css" />
+  <link rel="stylesheet/less" type="text/css" href="assets/css/nivade.less" />
+  <script src="assets/vendor/jquery/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/vendor/less/less.min.js" type="text/javascript"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="assets/vendor/angular/angular.min.js" type="text/javascript"></script>
+</head>
+
+</html>
